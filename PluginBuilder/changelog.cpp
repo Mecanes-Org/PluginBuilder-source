b0c1c8696219eab7b24e3df99377f65e8f625012
NEYLORXT <neylorxt@gmail.com>
2026-01-20 00:13:29 +0100

    v0.7.7 *New - Add ChangeLog [ Base ] - Update Settings UI - Update qss file - Small Update ...
    *Next
    - Add notifications ( software update, popup )
    - Add UI theme system

--- PluginBuilder/changelog.cpp
+++ PluginBuilder/changelog.cpp
@@ -1,6 +1,8 @@
 #include "changelog.h"
 #include "ui_changelog.h"
 
+#include <QPropertyAnimation>
+
 ChangeLog::ChangeLog(QWidget *parent)
     : QDialog(parent)
     , ui(new Ui::ChangeLog)
@@ -12,3 +14,28 @@ ChangeLog::~ChangeLog()
 {
     delete ui;
 }
+
+// void ChangeLog::on_pushButton_clicked()
+// {
+//     QWidget* widget_log = ui->widget_log;
+//     int height = widget_log->height();
+
+//     int targetHeight;
+//     if(height <= 60) {
+//         targetHeight = height * 3;
+//     } else {
+//         targetHeight = height / 3;
+//     }
+
+//     // Animation du widget qui se redimensionne
+//     QPropertyAnimation *animationSize = new QPropertyAnimation(widget_log, "geometry");
+//     animationSize->setDuration(300);
+//     animationSize->setStartValue(widget_log->geometry());
+
+//     QRect endGeometry = widget_log->geometry();
+//     endGeometry.setHeight(targetHeight);
+//     animationSize->setEndValue(endGeometry);
+//     animationSize->setEasingCurve(QEasingCurve::InOutQuad);
+
+//     animationSize->start(QAbstractAnimation::DeleteWhenStopped);
+// }
